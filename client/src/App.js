6de2fe5d207a5788a745e4aa00b258a43267b9cc
Svilla27 <svill069@fiu.edu>
2019-04-13 16:43:27 -0400

    added main navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
-import Registration from './Pages/registration'
+import React, { Component, Fragment } from 'react'
+// import Registration from './Pages/registration'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Navigation from './navigation'
+
+
 
 class App extends Component {
   render() {
     return (
-        <Registration />
+      // <BrowserRouter>
+      //   <Switch>
+      //     <Route exact path="/" component={Registration} />
+      //     <Route exact path="/home" component = {Home} />
+      //   </Switch>
+      // </BrowserRouter>
+         <Navigation />
     )
   }
 }
---
+++ client/src/Pages/application/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Application extends Component {
+
+    render() {
+        return (
+            <div>
+                Application
+            </div>
+        )
+    }
+}
+
+export default Application;
