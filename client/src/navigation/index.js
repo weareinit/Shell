778a7597610eeb912f110f16788c9e996a16bc23
jehfkemsy <jehf.denezaire@gmail.com>
2019-05-05 23:48:35 -0400

    fixing some logic issue

--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -5,12 +5,6 @@ import Auth from '../pages/auth'
 import Live from '../pages/live'
 import Landing from '../pages/landing'
 import Sponsor from '../pages/sponsor'
-//nested in dashboard
-import Application from '../components/application'
-import Home from '../components/home'
-import Schedule from '../components/schedule'
-import Faq from '../components/faq'
-import Profile from '../components/profile'
 import './style.css'
 class Navigation extends Component {
     constructor(props) {
@@ -32,14 +26,7 @@ class Navigation extends Component {
                         <Route exact path="/sponsor" component={Sponsor} />
                         <Route path="/dashboard"
                             render={({ match: { url } }) => (
-                                <div>
-                                    <Dashboard />
-                                    <Route path={`${url}/`} component={Home} exact />
-                                    <Route path={`${url}/application`} component={Application} />
-                                    <Route path={`${url}/schedule`} component={Schedule} />
-                                    <Route path={`${url}/profile`} component={Profile} />
-                                    <Route path={`${url}/faq`} component={Faq} />
-                                </div>
+                                <Dashboard url={url} />
                             )} />
                     </Switch>
                 </div>
--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import Navbar from '../../components/navbar'
+import { Route } from 'react-router-dom'
+//tabs 
+import Application from '../../components/application'
+import Home from '../../components/home'
+import Schedule from '../../components/schedule'
+import Faq from '../../components/faq'
+import Profile from '../../components/profile'
 import './styles.css'
 class Dashboard extends Component {
 
     render() {
+        let { url } = this.props;
         return (
             <div id="dashboard">
                 <Navbar />
+                <Route path={`${url}/`} component={Home} exact />
+                <Route path={`${url}/application`} component={Application} />
+                <Route path={`${url}/schedule`} component={Schedule} />
+                <Route path={`${url}/profile`} component={Profile} />
+                <Route path={`${url}/faq`} component={Faq} />
             </div>
         )
     }
