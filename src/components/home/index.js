e6e2b8f3413e22187d721a73427b4b4541ddf4f0
Svilla27 <svill069@fiu.edu>
2019-06-09 19:03:09 -0400

    added basic info into Home tab

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-
+import { individualData } from '../../data/home'
+import './styles.css'
 class Home extends Component {
 
     render() {
+        let { fullName, score, status } = individualData;
         return (
             <div id="home">
-               <h1 className="dashboard-page-title">Home</h1>
+                <h1 className="dashboard-page-title">Home</h1>
+
+                <img src='../../assets/frontpage/shell.svg' alt='avatar' />
+
+                <h2>Welcome, {fullName}!</h2>
+                <h3>Score: {score}</h3>
+
+                <h3>Application Status:</h3>
+                <button className='application-status-button'>
+                    <h3>{status}</h3>
+                </button>
             </div>
         )
     }
---
+++ src/data/home.js
@@ -0,0 +1,5 @@
+export const individualData = {
+    fullName : 'Andrew Scott',
+    score: 500,
+    status: 'Incomplete'
+};
