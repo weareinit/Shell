15e6a14578cc6717c0ccc385370968b008ec0e9e
jehfkemsy <jehf.denezaire@gmail.com>
2019-06-08 23:41:28 -0400

    added validation for application

---
+++ src/components/application/customSelect.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Select from 'react-select';
+
+
+class CustomSelect extends React.Component {
+
+    handleChange = async (value) => {
+        // this is going to call setFieldValue and manually update values.this.props.name
+        await this.props.onChange(this.props.name, value.value);
+        console.log(this.props)
+    };
+
+    handleBlur = async () => {
+        // this is going to call setFieldTouched and manually update touched.this.props.name
+        await this.props.onBlur(this.props.name, true);
+    };
+
+    render() {
+
+        let notIsValid = this.props.touched && this.props.error;
+
+        console.log(notIsValid)
+
+        const customStyles = {
+            control: (base, state) => ({
+                ...base,
+                // state.isFocused can display different borderColor if you need it
+                borderWidth: '2px',
+                borderStyle: ' solid ',
+                borderRadius: '8px',
+                borderColor: state.isFocused ?
+                    '#ddd' : notIsValid ?
+                        'red' : '#ddd',
+                // overwrittes hover style
+                '&:hover': {
+                    borderColor: state.isFocused ?
+                        '#ddd' : notIsValid ?
+                            'red' : '#ddd'
+                }
+            })
+        }
+        return (
+            <Select
+                styles={customStyles}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                className={this.props.className}
+                options={this.props.options}
+                setValue={this.props.value}
+                placeholder={this.props.placeholder}
+            />
+        )
+    }
+}
+
+export default CustomSelect;
