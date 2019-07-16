bb82026b0f5e3e07c60f950b52e64125370d42f4
jehfkemsy <jehf.denezaire@gmail.com>
2019-07-16 12:15:28 -0400

    added process env vars and preloaded icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
-import Navigation from "./navigation";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebookSquare,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+  faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faCheckSquare,
+  faSignOutAlt,
+  faExclamationCircle,
+  faLaptop,
+  faCar,
+  faSuitcaseRolling,
+} from "@fortawesome/free-solid-svg-icons";
+import dotenv from "dotenv";
 
-import "./css/App.css";
+import Navigation from "./navigation";
+import "./css/App.css"; //load general/global css
 
 //preloading icons
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faFacebookSquare, faInstagram, faLinkedin, faTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
-library.add(faCheckSquare, faSignOutAlt, faFacebookSquare, faInstagram, faLinkedin, faTwitter, faDiscord);
+library.add(
+  faCheckSquare,
+  faSignOutAlt,
+  faFacebookSquare,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+  faDiscord,
+  faExclamationCircle,
+  faLaptop,
+  faCar,
+  faSuitcaseRolling
+);
 
+//load enviroment vars...
+dotenv.config();
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       isLoggedIn: false,
-    }
-  };
+    };
+  }
 
   render() {
     return (
