5c4cc235b31e86affd618d0f7c2c1bfb7190fc90
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2019-08-14 01:19:45 -0400

    removed unecessary zindex

--- src/components/zoomableImg/index.jsx
+++ src/components/zoomableImg/index.jsx
@@ -13,13 +13,13 @@ const ZoomableImg = ({extraStyles, imageLink})=>{
         src: imageLink,
         alt: 'image',
         className: 'img',
-        style: { width: '100%', zIndex: 1000 }
+        style: { width: '100%' }
       }}
       defaultStyles={{
-        zoomContainer: { zIndex: 1000 },
+        zoomContainer: { },
         overlay: {
           /* default fallback */
-          background: 'rgbrgb(102, 91, 80) transparent',
+          background: 'rgb(102, 91, 80) transparent',
           /* modern browsers */
           backgroundColor: 'rgba(102, 91, 80,0.90000)'
         }
